d97ca6e8e60f73f3966e5d8c6f013915a3fe1a23
민철 (Codigm) <nice0612@naver.com>
2017-04-10 06:35:50 +0000

    add project

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+	var path = 'index.html';
+	var json = {
+		myinfo_display: '',
+		logout_display: '',
+		login_display: '',
+		signup_display: '',
+		mydata_display: ''
+	};
+	
+	var session = true;
+	if(session) {
+		json.login_display = 'display:none;';
+		json.signup_display = 'display:none;';
+	} else {
+		json.myinfo_display = 'display:none;';
+		json.logout_display = 'display:none;';
+		json.mydata_display = 'display:none;';
+	}
+	
+    res.render(path, json);
+};
