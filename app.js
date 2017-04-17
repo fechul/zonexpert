d0d250d0385c97fb1563910780effceadbd79643
devarrns <devarrns@gmail.com>
2017-04-17 15:06:06 +0000

    프로젝트 새로만듬

--- app.js
+++ app.js
@@ -1,38 +1,61 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var index = require('./routes/index');
 
 var app = express();
-var port = process.env.PORT || 3000;
 
-app.configure(function(){
-  app.set('port', port);
-  app.set('views', __dirname + '/views');
-  // app.set('view engine', 'ejs');
-  app.engine('html', require('ejs').renderFile);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	if (false) {
+		var err = new Error('Not Found');
+		err.status = 404;
+		next(err);
+	} else {
+		next();
+	}
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.send('error');
 });
+
+// routing
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('앱은 http://%s:%s 에서 작동 중입니다.', host, port);
 });
+
+
+module.exports = app;
