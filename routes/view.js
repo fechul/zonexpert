6a6d18e18762e53e299f9942b6a8c290945f1fd2
찌롱 <ghon0913@naver.com>
2017-05-10 14:10:24 +0000

    check

--- routes/view.js
+++ routes/view.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var board = require('../core/board.js');
 var user = require('../core/user.js');
 var schedule = require('../core/schedule.js');
-
+// 체크
 // 로그인 상태에서만 접속 가능한 페이지 체크
 // router.get('/url', need_login, function(req, res) {}) 형식으로 사용
 var need_login = function(req, res, next) {
