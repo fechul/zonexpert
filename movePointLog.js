59cd717efbf43f19a0dc28790352cf947ea09493
fechul <nice0612@gmail.com>
2017-08-01 02:18:00 +0900

    update script

--- movePointLog.js
+++ movePointLog.js
@@ -11,26 +11,38 @@ mongoose.connect('mongodb://localhost');
 
 db = require('./db/schema.js');
 
-db.user.find({}, function(err, data) {
-    async.each(data, function(user, async_cb) {
-        var pointLog = user.pointLog;
-        async.mapSeries(pointLog, function(log, _async_cb) {
-            log = JSON.stringify(log);
-            log = JSON.parse(log);
-            log.userEmail = user.email;
-            var moveLog = new db.point(log);
-            moveLog.save(function(_err, move) {
-                if(_err) {
-                    console.log("**********fail: ",user.email, _err);
-                } else {
-                    console.log("success!");
-                }
-                _async_cb();
-            });
-        }, function(_async_err) {
-            async_cb();
-        });
-    }, function(async_err) {
-        console.log("done!");
-    });
+// db.user.find({}, function(err, data) {
+//     async.each(data, function(user, async_cb) {
+//         var pointLog = user.pointLog;
+//         async.mapSeries(pointLog, function(log, _async_cb) {
+//             log = JSON.stringify(log);
+//             log = JSON.parse(log);
+//             log.userEmail = user.email;
+//             var moveLog = new db.point(log);
+//             moveLog.save(function(_err, move) {
+//                 if(_err) {
+//                     console.log("**********fail: ",user.email, _err);
+//                 } else {
+//                     console.log("success!");
+//                 }
+//                 _async_cb();
+//             });
+//         }, function(_async_err) {
+//             async_cb();
+//         });
+//     }, function(async_err) {
+//         console.log("done!");
+//     });
+// });
+
+db.user.update({}, {
+    $unset: {
+        'pointLog': 1
+    }
+}, {multi:true}, function(err, update) {
+    if(err) {
+        console.log("err: ", err);
+    } else {
+        console.log("suc: ", update);
+    }
 });
